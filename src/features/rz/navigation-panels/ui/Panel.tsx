34d40da260b5f8abfd6d0b73e3e885d746d39183
DanilP54 <putrodan@gmail.com>
2025-09-11 23:53:45 +0500

    refactor NavigationPanel component to streamline rendering logic, enhance accessibility attributes, and improve state management for preview and selected states

--- src/features/rz/navigation-panels/ui/Panel.tsx
+++ src/features/rz/navigation-panels/ui/Panel.tsx
@@ -2,10 +2,6 @@ import React, { RefObject, useEffect, useRef } from "react";
 import { useHintsStorage } from "../lib/useHintsStorage";
 import { Panel as TPanel } from "../types";
 import { useOnClickOutside } from "usehooks-ts";
-import {
-  NavLinksPreview,
-  NavLinksSelected,
-} from "@/features/rz/navigation-panels/ui/NavLinks";
 import { usePathname } from "next/navigation";
 import { toastHintManager } from "@/features/rz/navigation-panels/lib/toastHintManager";
 import { RZ_SEGMENTS } from "@/shared/model/routes";
@@ -36,6 +32,9 @@ export function NavigationPanel({
   const toast = toastHintManager();
   const previewRef = useRef<Nullable<HTMLElement>>(null);
 
+  const isPreview = state === "preview";
+  const isSelected = state === "selected";
+
   useEffect(() => {
     if (storage.isSeen(segment)) return;
 
@@ -55,9 +54,6 @@ export function NavigationPanel({
     handleClosePreview()
   );
 
-  const isPreview = state === "preview";
-  const isSelected = state === "selected";
-
   return (
     <div
       id={`${segment} panel`}
@@ -66,50 +62,8 @@ export function NavigationPanel({
       data-preview={state === "preview"}
       className={`group relative order-1 data-[selected=true]:order-0 h-[40px]`}
     >
-      <Toggle
-        data-pressed={isPreview}
-        aria-expanded={isPreview}
-        data-state={isPreview ? "on" : "off"}
-        aria-haspopup="true"
-        aria-controls="menu2"
-        onClick={previewToggle}
-        className={`${
-          isSelected ? "hidden" : "block"
-        } w-[35px] rounded-none cursor-pointer h-[40px] px-2 ${getBgColorOfSegment(
-          segment
-        )}`}
-      >
-        <ul
-          tabIndex={-1}
-          aria-label="Rodnaya Zemlya"
-          id="menu2"
-          className={`${isPreview ? "flex" : "hidden"} ${getBgColorOfSegment(
-            segment
-          )} flex-col justify-center gap-3 absolute top-0 left-[40px] right-2 z-50 shadow-lg text-white font-bold text-2xl px-4 py-1`}
-        >
-          <For each={panel.links}>
-            {(link) => (
-              <li key={link.label}>
-                <Link
-                  key={link.href}
-                  href={link.href}
-                  prefetch={false}
-                  className="flex items-center h-full"
-                >
-                  {link.label}
-                </Link>
-              </li>
-            )}
-          </For>
-        </ul>
-      </Toggle>
-      {isSelected && (
-        <NavLinksSelected
-          links={panel.links}
-          segment={segment}
-          pathname={pathname}
-        />
-      )}
+      {!isSelected && <DisclosureNavigationMenu panel={panel} isPreview={isPreview} onToggle={previewToggle}/>}
+      {isSelected && <NavigationMenu panel={panel} pathname={pathname} />}
     </div>
   );
 }
@@ -126,11 +80,12 @@ function DisclosureNavigationMenu({
   return (
     <>
       <Toggle
+        aria-label={`show navigation menu ${panel.name} segment`}
         data-pressed={isPreview}
         aria-expanded={isPreview}
         data-state={isPreview ? "on" : "off"}
         aria-haspopup="true"
-        aria-controls="menu2"
+        aria-controls="menu-list"
         onClick={onToggle}
         className={`w-[35px] rounded-none cursor-pointer h-[40px] px-2 ${getBgColorOfSegment(
           panel.name
@@ -138,8 +93,8 @@ function DisclosureNavigationMenu({
       >
         <ul
           tabIndex={-1}
-          aria-label="Rodnaya Zemlya"
-          id="menu2"
+          aria-label="rz navigation menu"
+          id="menu-list"
           className={`${isPreview ? "flex" : "hidden"} ${getBgColorOfSegment(
             panel.name
           )} flex-col justify-center gap-3 absolute top-0 left-[40px] right-2 z-50 shadow-lg text-white font-bold text-2xl px-4 py-1`}
@@ -181,13 +136,15 @@ function NavigationMenu({
       )} flex items-center justify-between gap-1 text-white font-bold w-full h-full border`}
     >
       <For each={panel.links}>
-        {(link) => (
-          <li
+        {(link) => {
+          const isActive = pathname.includes(link.href)
+          return <li
             key={link.href}
             className="w-full h-full flex items-center justify-center *:data-[active=true]:text-[20px] *:text-[9px]"
           >
             <Link
-              data-active={pathname.includes(link.href)}
+              aria-current={isActive}
+              data-active={isActive}
               href={link.href}
               prefetch={false}
               className="order-1  data-[active=true]:order-0 data-[active=true]:text-black data-[active=true]:pb-[4px]"
@@ -195,7 +152,7 @@ function NavigationMenu({
               {link.label}
             </Link>
           </li>
-        )}
+        }}
       </For>
     </ul>
   );
